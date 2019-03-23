87a5d46c3a616633928ee0bf725bbbb8cb83840e
MasterAddy <adesileisaiah70@gmail.com>
2019-03-23 17:02:25 +0100

    Updates

---
+++ precache-manifest.a8532a0dd1c187e1a1e081013415d24b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a665df6a6329d06e510d",
+    "url": "/routereact/static/css/main.411eda3f.chunk.css"
+  },
+  {
+    "revision": "a665df6a6329d06e510d",
+    "url": "/routereact/static/js/main.a665df6a.chunk.js"
+  },
+  {
+    "revision": "56317334fba6f4301cf9",
+    "url": "/routereact/static/js/runtime~main.56317334.js"
+  },
+  {
+    "revision": "606d1ffa482d53d646e2",
+    "url": "/routereact/static/js/2.606d1ffa.chunk.js"
+  },
+  {
+    "revision": "8cbac3b9c18595ce54ec579b2835d70b",
+    "url": "/routereact/index.html"
+  }
+];
